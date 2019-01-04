0773bb939519b8dc87803f7a450d6d6bb58593d2
alex <>
2019-01-04 15:49:44 -0500

    remove HelloWorld.vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,15 @@
 <template>
   <div id="app">
-    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <Scatterplot/>
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
 import Scatterplot from "./components/Scatterplot.vue";
 
 export default {
   name: "app",
   components: {
-    // HelloWorld,
     Scatterplot
   }
 };
